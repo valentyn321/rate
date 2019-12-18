71044fc1c56998115f50ee4b646ca4bd46fd80ab
valentyn <valentyncherkasov24@gmail.com>
2019-12-18 20:18:17 +0200

    Changed debug to False.

--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ class Profile(models.Model):
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
-        return f'{self.user.username}'
+        return self.user.username
 
     def save(self):
         super().save()
